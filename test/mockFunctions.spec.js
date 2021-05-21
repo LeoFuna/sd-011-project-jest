21102a67527c6be12567df5b9dc4ef13a46cd4a9
Lucas Maestrelli <lucasmaestrelli95@gmail.com>
2021-05-21 17:51:38 -0300

    req 3 lint

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -1,6 +1,6 @@
 const mockFunctions = require('../src/mockFunctions');
 
-jest.mock('../src/mockFunctions')
+jest.mock('../src/mockFunctions');
 /*
 Criamos uma série de funções com eficiência duvidosa.
 Elas estão no arquivo 'src/mockFunctions.js'.
@@ -18,8 +18,9 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 describe('verifica as funções e os mocks', () => {
   // Crie suas mock functions aqui
   mockFunctions.multiply.mockImplementation((a, b) => a * b);
-  mockFunctions.power.mockImplementation((a, b) => a ** b)
-  mockFunctions.factorial.mockImplementation((a) => (a <= 1 ? 1 : mockFunctions.factorial(a - 1) * a));
+  mockFunctions.power.mockImplementation((a, b) => a ** b);
+  mockFunctions.factorial.mockImplementation((a) =>
+    (a <= 1 ? 1 : mockFunctions.factorial(a - 1) * a));
   mockFunctions.add.mockImplementation((a, b) => a + b);
   mockFunctions.subtract.mockImplementation((a, b) => a - b);
   mockFunctions.divide.mockImplementation((a, b) => a / b);
