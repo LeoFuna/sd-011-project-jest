eba0666e8e3e39d8014f25274d3a58562726a387
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-05-21 19:17:52 -0300

    Add function mockApi

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -25,20 +25,20 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
   const requestReturn = {
-      gender: 'male',
-      name: {
-        first: 'Antônio',
-        last: 'Britto',
-      },
-      location: {
-        country: 'Brazil',
-      },
-      email: 'tunico@bol.com.br',
-      login: {
-        username: 'tunicao123',
-        password: '1234567890',
-      }
-    };
+    gender: 'male',
+    name: {
+      first: 'Antônio',
+      last: 'Britto',
+    },
+    location: {
+      country: 'Brazil',
+    },
+    email: 'tunico@bol.com.br',
+    login: {
+      username: 'tunicao123',
+      password: '1234567890',
+    },
+  };
 
   api.fetchURL = jest.fn().mockResolvedValue(requestReturn);
 
