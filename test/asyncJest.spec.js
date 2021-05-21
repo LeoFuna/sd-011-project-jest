49e53d14c490a6f4343adee2740adaec1eb1db66
gabiazevedo <gabiazevedoms@gmail.com>
2021-05-21 16:06:11 -0300

    Requisito 1 do projeto corrigido

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,4 +1,3 @@
-const assert = require('assert');
 const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
@@ -17,6 +16,7 @@ describe('o retorno do telefonema', () => {
   });
 
   test('ocupado', async () => {
-  await expect(answerPhone(false)).rejects.toThrow(new Error('Infelizmente não podemos atender...'));
-});
+    await expect(answerPhone(false)).rejects
+      .toThrow(new Error('Infelizmente não podemos atender...'));
+  });
 });
