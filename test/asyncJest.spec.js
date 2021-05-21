92dd7427a8a27997f7591ac244175ed26fadb812
Matheus Araújo <matheusaraujo.dev@gmail.com>
2021-05-21 16:36:33 -0300

    Ajusta o requisito 01

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,13 +12,9 @@ ATENÇÃO!!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('o retorno do telefonema', () => {
   test('atende', () => {
-    // assert.fail();
-    // Insira seu teste assíncrono aqui
-    return expect(answerPhone(true)).resolves.toBe('Oi!');
+    expect(answerPhone(true)).resolves.toBe('Oi!');
   });
   test('ocupado', () => {
-    // assert.fail();
-    // Insira seu teste assíncrono aqui
-    return expect(answerPhone(false)).rejects.toThrowError('Infelizmente não podemos atender...');
-  })
+    expect(answerPhone(false)).rejects.toThrowError('Infelizmente não podemos atender...');
+  });
 });
