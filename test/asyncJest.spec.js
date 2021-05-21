9f2157befe5eee4f725c83218441d3ca1b319ac9
Lucas Maestrelli <lucasmaestrelli95@gmail.com>
2021-05-21 16:16:17 -0300

    lint req1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,5 +1,5 @@
-const assert = require('assert');
-const { resolve } = require('path');
+// const assert = require('assert');
+// const { resolve } = require('path');
 const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
@@ -13,11 +13,9 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('o retorno do telefonema', () => {
   test('atende', async () => {
-    answerPhone
     expect(answerPhone(true)).resolves.toBe('Oi!');
   });
   test('ocupado', async () => {
-     expect(answerPhone(false)).rejects.toBe('Infelizmente não podemos atender...');
-
+    expect(answerPhone(false)).rejects.toBe('Infelizmente não podemos atender...');
   });
 });
