0c807c5c54fe96063ace58ab0a6a76a331e9374d
KaioHPs <kaiohpsalternativo@gmail.com>
2021-05-21 16:39:45 -0300

    Requisito 2

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,5 +1,4 @@
 const assert = require('assert');
-const { hasUncaughtExceptionCaptureCallback } = require('process');
 const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
