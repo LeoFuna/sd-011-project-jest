bee9d87b2e7b5e8e8fe097da30fdd15937ac4c97
benlopes <rubenssouss@hotmail.com>
2021-05-21 18:14:56 -0300

    1st requirement completed

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,18 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('o retorno do telefonema', () => {
   test('atende', () => {
-    assert.fail();
+    // assert.fail();
     // Insira seu teste assíncrono aqui
+    answerPhone(true).then((data) => {
+      expect(data).toBe('Oi!');
+    });
   });
+
   test('ocupado', () => {
-    assert.fail();
+    // assert.fail();
     // Insira seu teste assíncrono aqui
+    answerPhone(false).then((data) => {
+      expect(data).toThrow(new Error('Infelizmente não podemos atender...'));
+    });
   });
 });
