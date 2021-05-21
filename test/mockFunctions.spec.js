8ba365ff13e93e7e8f4e4104fc63ef8d04b5df93
heyset <c.inaciomatheus@gmail.com>
2021-05-21 16:41:28 -0300

    muda fatorial para one liner

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -22,11 +22,7 @@ describe('verifica as funções e os mocks', () => {
   mockFunctions.multiply = jest.fn((a, b) => a * b);
   mockFunctions.divide = jest.fn((a, b) => a / b);
   mockFunctions.power = jest.fn((a, b) => a ** b);
-  mockFunctions.factorial = jest.fn((n) => {
-    if (n === 1) return n;
-
-    return n * mockFunctions.factorial(n - 1);
-  });
+  mockFunctions.factorial = jest.fn((n) => (n === 1 ? n : mockFunctions.factorial(n - 1)));
 
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
