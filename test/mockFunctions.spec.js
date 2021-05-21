174e9c82c6f284846635adf5406162f0bc0c96e1
Lucas Maestrelli <lucasmaestrelli95@gmail.com>
2021-05-21 17:59:40 -0300

    req 4

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -16,7 +16,6 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica as funções e os mocks', () => {
-  // Crie suas mock functions aqui
   mockFunctions.multiply.mockImplementation((a, b) => a * b);
   mockFunctions.power.mockImplementation((a, b) => a ** b);
   mockFunctions.factorial.mockImplementation((a) =>
