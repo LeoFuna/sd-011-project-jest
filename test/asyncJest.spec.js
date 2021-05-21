e93cbe1dafee07030427519692a3a8ebffe63a19
LeoFuna <leo.funa@gmail.com>
2021-05-21 13:42:42 -0300

    Iniciando projeto task 1 VQV

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,15 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('o retorno do telefonema', () => {
   test('atende', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    // assert.fail();
+    expect.assertions(1);
+    return answerPhone(true).then((status) => expect(status).toBe('Oi!'));
   });
   test('ocupado', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    // assert.fail();
+    expect.assertions(1);
+    return answerPhone(false).catch((error) => {
+      expect(error).toEqual(new Error('Infelizmente não podemos atender...'));
+    });
   });
 });
