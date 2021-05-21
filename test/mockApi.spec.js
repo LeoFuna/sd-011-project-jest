a6251872a8af36b8a7ff6dcc3e852f06790f1022
BeatrizR <biarl.ramalhorl@gmail.com>
2021-05-21 15:53:18 -0300

    corrigindo ;

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -1,6 +1,6 @@
 const api = require('../src/mockApi');
 
-jest.mock('../src/mockApi')
+jest.mock('../src/mockApi');
 
 /*
 A função fetchURL retorna um JSON com informações de um usuário aleatório buscadas da API 'randomuser.me'.
