9739cad8b9ace5383f59c0174243d593400057c2
Renan <renan.vamo@gmail.com>
2021-05-21 19:26:08 -0300

    Add o ex3, mockApi

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -24,10 +24,26 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
-
-  test('verifica se o usuário é o tunico', async () => (
+  const fetchURL = jest.spyOn(api, 'fetchURL');
+  const userReturned = {
+    gender: 'Male',
+    name: {
+      first: 'Antônio',
+      last: 'Britto',
+    },
+    location: {
+      country: 'Brazil',
+    },
+    email: 'tunico@bol.com.br',
+    login: {
+      username: 'tunicao123',
+      password: '1234567890',
+    },
+  };
+  api.fetchURL.mockResolvedValue(userReturned);
+  test('verifica se o usuário é o tunico', () => (
     api.fetchURL().then((user) => {
-      expect(user.gender).toEqual('male');
+      expect(user.gender).toEqual('Male');
       expect(user.name.first).toEqual('Antônio');
       expect(user.name.last).toEqual('Britto');
       expect(user.location.country).toEqual('Brazil');
