ee83866229e545fb46f4efa988c6c23c5e8fd9df
Thiago Cavalcante do Espirito Santo <thiagofox00@gmail.com>
2021-05-21 18:42:46 -0300

    requisito 1 e 4 finalizados

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const { hasUncaughtExceptionCaptureCallback } = require('process');
 const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
@@ -11,13 +12,28 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('o retorno do telefonema', () => {
-  test('atende', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test('atende', async () => {
+    expect.assertions(1);
+    const response = answerPhone(true);
+    return expect(response).resolves.toBe('Oi!');
   });
-  test('ocupado', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test('ocupado', async () => {
+    expect.assertions(1);
+    const response = answerPhone(false);
+    return expect(response).rejects.toThrowError('Infelizmente não podemos atender...');
   });
 });
-// teste
+
+describe('o retorno do telefonema', () => {
+  test('atende', () => {
+    expect.assertions(1);
+    return answerPhone(true).then((response) => {
+      expect(response).toBe('Oi!');
+    });
+  });
+  test('ocupado', () => {
+    expect.assertions(1);
+    return answerPhone(false).catch((response) =>
+      expect(response).toEqual(new Error('Infelizmente não podemos atender...')));
+  });
+});
