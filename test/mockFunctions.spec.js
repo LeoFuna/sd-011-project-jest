df3a6a0eb98ec55b6ea1afe0eb7f1ff7e30534ea
heyset <c.inaciomatheus@gmail.com>
2021-05-21 17:01:30 -0300

    Corrige fatoria

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -22,7 +22,7 @@ describe('verifica as funções e os mocks', () => {
   mockFunctions.multiply = jest.fn((a, b) => a * b);
   mockFunctions.divide = jest.fn((a, b) => a / b);
   mockFunctions.power = jest.fn((a, b) => a ** b);
-  mockFunctions.factorial = jest.fn((n) => (n === 1 ? n : mockFunctions.factorial(n - 1)));
+  mockFunctions.factorial = jest.fn((n) => (n === 1 ? n : n * mockFunctions.factorial(n - 1)));
 
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
