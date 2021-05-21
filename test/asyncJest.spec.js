629ddd97c413dfecd1103da633320cb162c7b3f2
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-05-21 15:46:54 -0300

    refatora requisito 01

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,8 +11,7 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('o retorno do telefonema', () => {
   test('atende', async () => {
-    const result = answerPhone(true);
-    await expect(result).resolves.toBe('Oi!');
+    await expect(answerPhone(true)).resolves.toBe('asdf!');
   });
   test('ocupado', async () => {
     const result = answerPhone(false);
