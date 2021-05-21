d3ce4dc6e2d9e3237fb748486bf3eac1caccf58e
Hamander <1hamander@gmail.com>
2021-05-21 14:53:40 -0300

    Requisito_1 Completo

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,13 +11,13 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('o retorno do telefonema', () => {
-  test('atende', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  it('atende', async () => {
+    const trueData = answerPhone(true);
+    await expect(trueData).resolves.toEqual('Oi!');
   });
-  test('ocupado', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    it('ocupado', async () => {
+      const falseData = answerPhone(false);
+      await expect(falseData).rejects.toThrow('Infelizmente não podemos atender...');
   });
 });
 //
