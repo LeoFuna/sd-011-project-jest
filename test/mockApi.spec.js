d2042154b24e99215b61c3627b2c61a28dec0fc0
Gisele Costa <jikinha@gmail.com>
2021-05-21 16:10:00 -0300

    lint no mockApi

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -23,16 +23,16 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica o usuário', () => {
-  const user = {
+  const newUser = {
     gender: 'male',
     name: { first: 'Antônio', last: 'Britto' },
     location: { country: 'Brazil' },
     email: 'tunico@bol.com.br',
-    login: { username: 'tunicao123', password: '1234567890' }
+    login: { username: 'tunicao123', password: '1234567890' },
   };
 
-  api.fetchURL = jest.fn().mockResolvedValue(user);
-  
+  api.fetchURL = jest.fn().mockResolvedValue(newUser);
+
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
       expect(user.gender).toEqual('male');
