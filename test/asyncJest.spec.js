541f4796a115a8008b00aedd760766b0dfabef15
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-05-21 15:15:44 -0300

    correcoes lint

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,4 +1,5 @@
-const assert = require('assert');
+// const assert = require('assert');
+
 const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
@@ -13,10 +14,11 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'
 describe('o retorno do telefonema', () => {
   test('atende', async () => {
     expect.assertions(1);
-      await expect(answerPhone(true)).resolves.toBe('Oi!');
+    await expect(answerPhone(true)).resolves.toBe('Oi!');
   });
   test('ocupado', async () => {
     expect.assertions(1);
-      await expect(answerPhone(false)).rejects.toEqual(new Error('Infelizmente não podemos atender...'));
+    await expect(answerPhone(false))
+      .rejects.toEqual(new Error('Infelizmente não podemos atender...'));
   });
 });
