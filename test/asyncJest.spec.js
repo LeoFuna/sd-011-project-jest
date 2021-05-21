ef11388ad0a7af19272c989c3f6b5d1952357651
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-05-21 14:56:43 -0300

    adiciona requisito 01

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,4 +1,3 @@
-const assert = require('assert');
 const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
@@ -11,12 +10,12 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('o retorno do telefonema', () => {
-  test('atende', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test('atende', async () => {
+    const result = answerPhone(true);
+    await expect(result).resolves.toBe('Oi!');
   });
-  test('ocupado', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test('ocupado', async () => {
+    const result = answerPhone(false);
+    await expect(result).rejects.toThrow(new Error('Infelizmente não podemos atender...'));
   });
 });
