ab269e12f2e5a489434cdebae15d0e7af2fcf053
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2021-05-21 20:24:16 -0300

    Requisito 4

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -21,20 +21,28 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('quem sobreviveu?', () => {
   // Adicione seu código aqui
+  beforeEach(() => { // garante que a função vai sendo chamada em cada teste
+    adventure.randomAttack();
+  });
 
   test('depois da primeira aventura', () => {
+    expect.assertions(1);
     expect(adventure.specialists.length).toBe(5);
   });
   test('depois da segunda aventura', () => {
+    expect.assertions(1);
     expect(adventure.specialists.length).toBe(4);
   });
   test('depois da terceira aventura', () => {
+    expect.assertions(1);
     expect(adventure.specialists.length).toBe(3);
   });
   test('depois da quarta aventura', () => {
+    expect.assertions(1);
     expect(adventure.specialists.length).toBe(2);
   });
   test('depois da quinta aventura', () => {
+    expect.assertions(1);
     expect(adventure.specialists.length).toBe(1);
   });
 });
