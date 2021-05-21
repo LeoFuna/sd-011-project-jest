ac89a8eaa2ab7138e7fdd0e330f79b5df35be445
Hamander <1hamander@gmail.com>
2021-05-21 14:55:24 -0300

    Lint Fixes

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,4 +1,3 @@
-const assert = require('assert');
 const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
@@ -15,9 +14,9 @@ describe('o retorno do telefonema', () => {
     const trueData = answerPhone(true);
     await expect(trueData).resolves.toEqual('Oi!');
   });
-    it('ocupado', async () => {
-      const falseData = answerPhone(false);
-      await expect(falseData).rejects.toThrow('Infelizmente não podemos atender...');
+  it('ocupado', async () => {
+    const falseData = answerPhone(false);
+    await expect(falseData).rejects.toThrow('Infelizmente não podemos atender...');
   });
 });
 //
