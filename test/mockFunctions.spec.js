2bb195cf49a9a5cbfe140bce845b348d412b0fc2
josanjohnata <josanjohnata@gmail.com>
2021-05-21 16:39:00 -0300

    requisito 2 function 2

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -16,6 +16,7 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica as funções e os mocks', () => {
   mockFunctions.add.mockImplementation((num1, num2) => num1 + num2);
+  mockFunctions.subtract.mockImplementation((num1, num2) => num1 - num2);
 
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
