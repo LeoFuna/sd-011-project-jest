dd11ff1ef6ec4058d9031c87b6752a6ad606076b
gabrielfndz <gabriel.omercador@hotmail.com>
2021-05-21 16:38:44 -0300

    Primeiro Requisito

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,13 +12,9 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('o retorno do telefonema', () => {
   test('atende', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    expect(answerPhone(true)).resolves.toBe('Oi!');
   });
   test('ocupado', () => {
-    assert.fail();
-
-    
-    // Insira seu teste assíncrono aqui
+    expect(answerPhone(false)).rejects.toThrowError('Infelizmente não podemos atender...');
   });
 });
