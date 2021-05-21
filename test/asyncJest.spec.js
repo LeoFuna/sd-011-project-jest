d0d03751ccccecd114988be8b4bf70e16e12ad74
Felipe Buso <felipe_buso@hotmail.com>
2021-05-21 15:41:34 -0300

    Item 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,13 +11,9 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('o retorno do telefonema', () => {
-  test('atende', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
-  });
+  test('atende', () => { expect(answerPhone(true)).resolves.toBe('Oi!'); });
+
   test('ocupado', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    expect(answerPhone(false)).rejects.toThrow(Error('Infelizmente não podemos atender...'));
   });
-  
 });
