66f75592e488d6048171dd8dfc372a5473f5641f
Leandro Lopes <leandro_carlos@uol.com.br>
2021-05-21 20:20:36 -0300

    exercicio 3

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -19,11 +19,19 @@ presente no README.md do projeto.
 
 Dica: Utilizem os métodos jest.fn() ou jest.spyOn().
 
-ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
+ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'..
 */
 
 describe('verifica o usuário', () => {
-  // Crie sua mock da função fetchURL() aqui
+  api.fetchURL = jest.fn().mockResolvedValue({
+    gender: 'male',
+    name: { first: 'Antônio', last: 'Britto' },
+    location: {
+      country: 'Brazil',
+    },
+    email: 'tunico@bol.com.br',
+    login: { username: 'tunicao123', password: '1234567890' },
+  });
 
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
