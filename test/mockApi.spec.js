0ee5003a2dc6c47a0a94ff3e0bcba0f2295d2767
Ghislaine <glenox79@gmail.com>
2021-05-21 16:34:07 -0300

    Correcao exer - 3

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -41,10 +41,11 @@ describe('verifica o usuário', () => {
     },
   };
 
+  api.fetchURL = jest.fn().mockResolvedValue(userTunico);
+
   test('verifica se o usuário é o tunico', async () => {
     expect.assertions(7);
-    api.fetchURL = jest.fn().mockResolvedValue(userTunico);
-    api.fetchURL().then((user) => {
+    return api.fetchURL().then((user) => {
       expect(user.gender).toEqual('male');
       expect(user.name.first).toEqual('Antônio');
       expect(user.name.last).toEqual('Britto');
