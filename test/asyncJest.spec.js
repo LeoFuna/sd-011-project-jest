8c1eabf4510c49d42f72c75ebb418c6865974eb0
heyynat <natalifplima@gmail.com>
2021-05-21 14:56:42 -0400

    Requisito 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,13 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('o retorno do telefonema', () => {
   test('atende', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    answerPhone(true).then(msg => {
+      expect(msg).toEqual('Oi!');
+    })
   });
   test('ocupado', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    answerPhone(false).catch(msg => {
+      expect(msg).toEqual(new Error('Infelizmente não podemos atender...'));
+    })
   });
 });
