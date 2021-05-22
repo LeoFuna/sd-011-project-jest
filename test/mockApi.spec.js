305b19fd6fdbe3dbcebf9ed93af2ba7d4b98f20e
Cristian Brum Oliveira <cristian.brum2@yahoo.com.br>
2021-05-21 23:55:24 -0300

    requisito 3

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -23,8 +23,15 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica o usuário', () => {
-  // Crie sua mock da função fetchURL() aqui
-
+  api.fetchURL = jest.fn().mockResolvedValue(
+    {
+      gender: 'male',
+      name: { first: 'Antônio', last: 'Britto' },
+      location: { country: 'Brazil' },
+      email: 'tunico@bol.com.br',
+      login: { username: 'tunicao123', password: '1234567890' },
+    },
+  );
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
       expect(user.gender).toEqual('male');
@@ -34,6 +41,7 @@ describe('verifica o usuário', () => {
       expect(user.email).toEqual('tunico@bol.com.br');
       expect(user.login.username).toEqual('tunicao123');
       expect(user.login.password).toEqual('1234567890');
+      expect(api.fetchURL).toHaveBeenCalledTimes(1);
     })
   ));
 });
