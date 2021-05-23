26bac8a14e0b017ec12482a1c9d304b0554ed7fd
Maran <maranmorimoto@gmail.com>
2021-05-23 17:34:09 -0300

    mockFunctions and setupTeardown requests have been done

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -1,5 +1,7 @@
 const api = require('../src/mockApi');
 
+// jest.mock('../src/mockApi'); Uso o jest.mock('../src/mockApi') quando quero simular 
+// várias funções no arquivo linkado('../src/mockApi'). Portato se eu quiser fazer simulação pra uma função só é melhor colocar 
 /*
 A função fetchURL retorna um JSON com informações de um usuário aleatório buscadas da API 'randomuser.me'.
 No entanto, nos testes abaixo, queremos que todas as vezes que chamarmos a API a resposta contenha as informações do nosso adminis..Cof! Cof!.. programador favorito, Tunicão.
@@ -23,8 +25,17 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica o usuário', () => {
-  // Crie sua mock da função fetchURL() aqui
-
+  const trybeAdmin = { gender: 'male',
+    name: { first: 'Antônio', last: 'Britto' },
+    location: { country: 'Brazil' },
+    email: 'tunico@bol.com.br',
+    login: { username: 'tunicao123', password: '1234567890' },
+  };
+  
+  const spy = jest.spyOn(api,"fetchURL");
+  // api.fetchURL = jest.fn();
+  api.fetchURL.mockResolvedValue(trybeAdmin);
+  
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
       expect(user.gender).toEqual('male');
