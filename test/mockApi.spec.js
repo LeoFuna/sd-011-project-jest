ceb1aa608f8b185be52eae9989cc538b78026770
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-05-23 03:07:53 -0300

    task 2

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -22,8 +22,19 @@ Dica: Utilizem os métodos jest.fn() ou jest.spyOn().
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
+jest.mock('../src/mockApi');
+
 describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
+  // Reference: https://jestjs.io/docs/mock-function-api#mockfnmockresolvedvaluevalue
+  api.fetchURL = jest.fn().mockResolvedValue( {
+      gender: 'male',
+      name: { first: 'Antônio', last: 'Britto' },
+      location: { country: 'Brazil'},
+      email: 'tunico@bol.com.br',
+      login: { username: 'tunicao123', password: '1234567890'},
+    }
+  )
 
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
