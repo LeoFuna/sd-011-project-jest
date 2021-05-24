1e600a3dfe1879dffea7f0884a07ead9985e44ec
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-05-24 09:38:15 -0300

    Remove blank line

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -20,7 +20,6 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('quem sobreviveu?', () => {
-
   // Será executado para cada teste dentro deste bloco "describe"
   beforeEach(() => {
     adventure.randomAttack();
