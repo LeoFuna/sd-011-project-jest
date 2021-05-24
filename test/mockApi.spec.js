e06573fe72f618aaed112beff6a240853a316009
Bernardo Prado <bernardoprado22@yahoo.com.br>
2021-05-24 20:38:26 -0300

    Corrige lint

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -25,23 +25,23 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 describe('verifica o usuário', () => {
   const UserInfo = ({
     gender: 'male',
-    name: { 
-      first: 'Antônio', 
-      last: 'Britto' 
+    name: {
+      first: 'Antônio',
+      last: 'Britto',
     },
 
-    location: { 
-        country: 'Brazil' 
+    location: {
+      country: 'Brazil',
     },
 
     email: 'tunico@bol.com.br',
-    login: { 
-      username: 'tunicao123', 
-      password: '1234567890' 
+    login: {
+      username: 'tunicao123',
+      password: '1234567890',
     },
-});
+  });
 
-jest.spyOn(api, 'fetchURL').mockResolvedValue(UserInfo);
+  jest.spyOn(api, 'fetchURL').mockResolvedValue(UserInfo);
 
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
