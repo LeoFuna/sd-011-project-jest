e3da28a5713cfa7b9b30696079e5f9825136a910
Luana Souza <luanah.souzza@hotmail.com>
2021-05-25 20:43:54 -0300

    Corrige erro Requisito 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -10,8 +10,6 @@ a função recebe como parâmetro true e false, respectivamente.
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
-const answerPhone = require('../src/asyncJest');
-
 describe('o retorno do telefonema', () => {
   test('atende', async () => {
     expect.assertions(1);
