52462ac9d7248f4472c6640c60a243c56fa88393
Gustavo Mendes <gusm217@gmail.com>
2021-05-25 20:10:22 -0300

    Finaliza Jest Project

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -22,6 +22,9 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 describe('quem sobreviveu?', () => {
   // Adicione seu código aqui
 
+  beforeEach(() => {
+    adventure.randomAttack();
+  });
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
   });
@@ -36,5 +39,6 @@ describe('quem sobreviveu?', () => {
   });
   test('depois da quinta aventura', () => {
     expect(adventure.specialists.length).toBe(1);
+    console.log(`E o fi de Deus q sobrou foi: ${adventure.specialists[0].nome}`);
   });
 });
