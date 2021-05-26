c1b0bf3091c85a6bb629654abdab3ade7207a9ef
Gabriel <gribeiro237@gmail.com>
2021-05-26 19:21:39 -0300

    requsito 3 refeito

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -19,7 +19,13 @@ describe('verifica as funções e os mocks', () => {
   mockFunctions.subtract.mockImplementation((a, b) => a - b);
   mockFunctions.multiply.mockImplementation((a, b) => a * b);
   mockFunctions.divide.mockImplementation((a, b) => a / b);
-  mockFunctions.power.mockImplementation((a, b) => a ** b);
+  mockFunctions.power.mockImplementation((a, b) => {
+    let pow = a;
+    for (let i = 0; i < b; i += 1) {
+      pow = (a ** b);
+    }
+    return pow;
+  });
   mockFunctions.factorial.mockImplementation((a) => {
     let fact = 1;
     for (let i = 2; i <= a; i += 1) {
