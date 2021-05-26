b3b91a3b0b92c9acba6be599078de692ace43637
igordmf <igordanielmf@gmail.com>
2021-05-26 18:53:35 -0300

    Adiciona requisito 2

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -23,7 +23,7 @@ describe('verifica as funções e os mocks', () => {
   mockFunctions.subtract.mockImplementation((a, b) => a - b);
   mockFunctions.multiply.mockImplementation((a, b) => a * b);
   mockFunctions.divide.mockImplementation((a, b) => a / b);
-  mockFunctions.power.mockImplementation((a, b) => Math.pow(a, b));
+  mockFunctions.power.mockImplementation((a, b) => a ** b);
   mockFunctions.factorial.mockImplementation((a) => {
     let fact = 1;
     if (a === 0 || a === 1) { return fact; }
