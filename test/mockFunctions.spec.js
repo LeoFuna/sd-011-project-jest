99e318a1b833f1d72890f5b9297c2582d8855c0e
Gabriel <gribeiro237@gmail.com>
2021-05-26 18:40:26 -0300

    terceiro requisito

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -15,7 +15,25 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica as funções e os mocks', () => {
-  // Crie suas mock functions aqui
+  mockFunctions.add.mockImplementation((a, b) => a + b);
+  mockFunctions.subtract.mockImplementation((a, b) => a - b);
+  mockFunctions.multiply.mockImplementation((a, b) => a * b);
+  mockFunctions.divide.mockImplementation((a, b) => a / b);
+  mockFunctions.power.mockImplementation((a, b) =>{
+      let pow = a;
+      for (let i = 0; i < b; i += 1) {
+        pow = multiply(pow, a);
+      }
+      return pow;
+  });
+  mockFunctions.factorial.mockImplementation((a) =>{
+    let fact = 1;
+      for (let i = 2; i <= a; i += 1) {
+        fact *= i;
+      }
+      return fact;
+    });
+  });
 
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
