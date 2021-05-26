fb2f6f99f78d74f6de4d7db767082177e5f87315
Luana Souza <luanah.souzza@hotmail.com>
2021-05-25 21:18:12 -0300

    Adiciona Requisito 3

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -23,7 +23,17 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica o usuário', () => {
-  // Crie sua mock da função fetchURL() aqui
+  const expectedUser = {
+    gender: 'male',
+    name: { first: 'Antônio', last: 'Britto' },
+    location: { country: 'Brazil' },
+    email: 'tunico@bol.com.br',
+    login: { username: 'tunicao123', password: '1234567890' },
+  };
+
+  // api.fetchURL = jest.fn().mockResolvedValue(expectedUser);
+
+  jest.spyOn(api, 'fetchURL').mockResolvedValue(expectedUser);
 
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
@@ -37,3 +47,4 @@ describe('verifica o usuário', () => {
     })
   ));
 });
+
