5ab36a741734fc5fce353540703025f67237815c
JoaoCabrera <joaocabrerati@gmail.com>
2021-05-26 09:58:43 -0300

    Corrige lint

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,10 +11,10 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('o retorno do telefonema', () => {
-  test('atende', async() => {
+  test('atende', async () => {
     await expect(answerPhone(true)).resolves.toEqual('Oi!');
   });
-  test('ocupado', async() => {
-    await expect(answerPhone(false)).rejects.toThrow('Infelizmente não podemos atender...')
+  test('ocupado', async () => {
+    await expect(answerPhone(false)).rejects.toThrow('Infelizmente não podemos atender...');
   });
 });
