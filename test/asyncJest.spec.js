476946ebebb75a98b1066fb200b13ee3aa0dc1cb
Taís Carvalho <taispcarvalho@gmail.com>
2021-06-09 13:08:44 -0300

    1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,12 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('o retorno do telefonema', () => {
-  test('atende', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test('atende', async () => {
+    // assert.fail();
+    expect(answerPhone(true)).resolves.toBe('Oi!');
   });
-  test('ocupado', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test('ocupado', async () => {
+    // assert.fail();
+    expect(answerPhone(false)).rejects.toThrowError(new Error('Infelizmente não podemos atender...'));
   });
 });
