2a3941a93ea069e2174f9af06df1f1f3b5fbe862
Anibal <anibalneto29@gmail.com>
2021-06-10 06:17:15 -0300

    Requisito 4

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -24,6 +24,24 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
+  const spyApi = jest.spyOn(api, 'fetchURL');
+
+  const tunico = {
+    gender: 'male',
+    name: { first: 'Antônio', last: 'Britto' },
+    location: {
+      country: 'Brazil',
+    },
+    email: 'tunico@bol.com.br',
+    login: {
+      username: 'tunicao123',
+      password: '1234567890',
+    },
+  };
+
+  spyApi.mockResolvedValue(tunico);
+
+  // Source: consulta ao repositório do Matheus Gaspar: https://github.com/tryber/sd-011-project-jest/pull/112/
 
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
