99b2d47df7f98d2d46fa61ccd9a5dbbb4803bbe1
LukeCruz <enio.cruz@yahoo.com.br>
2021-06-11 09:50:44 -0300

    req 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,6 +1,6 @@
 const answerPhone = require('../src/asyncJest');
 
-describe('o retorno do telefonema', () => {
+/*describe('o retorno do telefonema', () => {
   test('atende', async () => {
     expect.assertions(1);
     await expect(answerPhone(true)).resolves.toBe('Oi!');
@@ -9,4 +9,15 @@ describe('o retorno do telefonema', () => {
     expect.assertions(1);
     await expect(answerPhone(false)).rejects.toBe('Infelizmente não podemos atender...');
   });
+});*/
+
+describe('o retorno do telefonema', () => {
+  test('atende', async () => {
+    await expect(answerPhone(true)).resolves.toBe('Oi!');
+  });
+  test('ocupado', async () => {
+    await expect(answerPhone(false)).rejects.toThrowError('Infelizmente não podemos atender...');
+  });
 });
+
+
